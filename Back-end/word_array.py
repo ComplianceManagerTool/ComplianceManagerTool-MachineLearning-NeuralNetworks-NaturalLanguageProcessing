ed2761c7b5eafed69071f7c81121a766dc66ebea
shwetakothari <shweta.a.kothari@gmail.com>
2018-08-30 17:05:28 -0700

    created 2 folders and added matching words algo in backend

---
+++ Back-end/word_array.py
@@ -0,0 +1,23 @@
+# do pyton3 name.py to run this file
+from sklearn.feature_extraction.text import CountVectorizer
+# list of text documents
+text = ["The quick brown fox jumped over the lazy dog."]
+# create the transform
+vectorizer = CountVectorizer()
+# tokenize and build vocab
+vectorizer.fit(text)
+# summarize
+print(vectorizer.vocabulary_)
+# encode document
+vector = vectorizer.transform(text)
+# summarize encoded vector
+print(vector.shape)
+print(type(vector))
+print(vector.toarray())
+#####################################
+# encode another document
+text2 = ["the puppy"]
+vector = vectorizer.transform(text2)
+print(vector.toarray())
+
+
